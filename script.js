83b3019828f0a59506db16be55932a11436ea311
Ligengxin <Ligengxin96@gmail.com>
2023-06-29 12:37:45 +0800

    update

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ const bot = new TelegramBot(TOKEN, {
   },
 });
 
-const weatherImage = './weather.png';
+const location = "深圳市南山区";
+const weatherImage = `./${location}.png`;
 
 const getWeatherData = async (redoCount = 0) => {
   if (redoCount > 3) {
@@ -21,10 +22,10 @@ const getWeatherData = async (redoCount = 0) => {
   }
 
   try {
-    await exec(`curl -H "Accept-Language: zh-CN" "https://wttr.in/深圳市南山区_m1.png" --output ${weatherImage}`);
+    await exec(`curl -H "Accept-Language: zh-CN" "https://wttr.in/${location}_m1.png" --output ${weatherImage}`);
     console.log(dateFormat(), `获取天气图片成功.`);
 
-    const { stdout } = await exec(`curl -H "Accept-Language: zh-CN" "https://wttr.in/深圳市南山区?m1&format=j1"`);
+    const { stdout } = await exec(`curl -H "Accept-Language: zh-CN" "https://wttr.in/${location}?m1&format=j1"`);
     console.log(dateFormat(), `获取天气json数据成功.`);
 
     const weatherData = JSON.parse(stdout);
@@ -45,17 +46,17 @@ const getWeatherReport = async () => {
     bot.sendMessage(AUTH_USER_ID, '获取天气失败.');
     return;
   } else {
-    bot.sendMessage(AUTH_USER_ID, `现在是 ${dateFormat()}, 当前深圳市南山区的天气为 ${weatherDesc},最高温度为 ${maxtempC} 度, 最低温度为 ${mintempC} 度`);
+    bot.sendMessage(AUTH_USER_ID, `现在是 ${dateFormat()}\n今天${location}:${weatherDesc}\n最高温度: ${maxtempC} 度, 最低温度: ${mintempC} 度`);
     bot.sendPhoto(AUTH_USER_ID, weatherImage);
   }
 }
 
 const checkIn = async () => {
-  bot.sendMessage(AUTH_USER_ID, `现在是 ${dateFormat()} ,记得上班打卡哦`);
+  bot.sendMessage(AUTH_USER_ID, `现在是 ${dateFormat()}\n记得上班打卡哦`);
 }
 
 const checkOut = async () => {
-  bot.sendMessage(AUTH_USER_ID, `现在是 ${dateFormat()} ,记得下班打卡哦`);
+  bot.sendMessage(AUTH_USER_ID, `现在是 ${dateFormat()}\n记得下班打卡哦`);
 }
 
 module.exports = {
