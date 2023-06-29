da245f4bba275c270ae78670241eff707bb88c52
Ligengxin <Ligengxin96@gmail.com>
2023-06-29 09:26:51 +0800

    bug fix

--- util.js
+++ util.js
@@ -1,6 +1,6 @@
 const moment = require('moment');
 
-const dateFormat = (date = moment().add(12, 'hours'), format = 'YYYY-MM-DD HH:mm:ss') => {
+const dateFormat = (date = moment(), format = 'YYYY-MM-DD HH:mm:ss') => {
   return moment(date).format(format);
 }
 
